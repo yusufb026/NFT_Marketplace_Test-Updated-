182a7dd4a4c4adb55659c20d63e526d9733e3f9e
yusufb026 <yusufbayim26@gmail.com>
2025-01-15 04:02:10 -0800

    rePrettier

--- server/models/SocketMapping.js
+++ server/models/SocketMapping.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 //later we have to do mapping through redis server
 const socketMappingSchema = new mongoose.Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "admin",
-    },
-    socketId: String,
+   user: {
+      type: Schema.Types.ObjectId,
+      ref: "admin",
+   },
+   socketId: String,
 });
 module.exports = mongoose.model("socketmapping", socketMappingSchema);
--- server/models/SuggestKeywords.js
+++ server/models/SuggestKeywords.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const suggestKeywordSchema = mongoose.Schema(
-    {
-        keyword: {
-            type: String,
-            unique: true,
-        },
-        isDeleted: {
-            type: Date,
-            default: null,
-        },
-    },
-    { timestamps: true }
+   {
+      keyword: {
+         type: String,
+         unique: true,
+      },
+      isDeleted: {
+         type: Date,
+         default: null,
+      },
+   },
+   { timestamps: true }
 );
 module.exports = mongoose.model("suggestkeyword", suggestKeywordSchema);
