04c3c4605071feb6c1052b4126a0135e4ea602fe
Aydin Aydeniz <yusufbayim26@gmail.com>
2025-01-16 17:39:49 +0200

    UpdateServerCode

--- server/middlewares/helpers/fileRemover.js
+++ server/middlewares/helpers/fileRemover.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = (files) => {
-	return Promise.all(
-		files.map(
-			(file) =>
-				new Promise((res, rej) => {
-					try {
-						setTimeout(() => {
-							fs.unlink(file, (err) => {
-								if (err) throw err;
-								res();
-							});
-						}, 10000);
-					} catch (err) {
-						console.error(err);
-						rej(err);
-					}
-				})
-		)
-	);
+    return Promise.all(
+        files.map(
+            (file) =>
+                new Promise((res, rej) => {
+                    try {
+                        setTimeout(() => {
+                            fs.unlink(file, (err) => {
+                                if (err) throw err;
+                                res();
+                            });
+                        }, 10000);
+                    } catch (err) {
+                        console.error(err);
+                        rej(err);
+                    }
+                })
+        )
+    );
 };
